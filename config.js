7816b37e9e60952a2a0612b093e19006cf57d196
MH-Lanches <mhlanches.igarassu@gmail.com>
2026-04-11 00:53:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyBXkN1UfotGkxj_ucnBnlZCBQBoGWykHZM",
+    authDomain: "cardapiomhlanches.firebaseapp.com",
+    databaseURL: "https://cardapiomhlanches-default-rtdb.firebaseio.com",
+    projectId: "cardapiomhlanches",
+    storageBucket: "cardapiomhlanches.firebasestorage.app",
+    messagingSenderId: "919725344771",
+    appId: "1:919725344771:web:69be07a143162e76cf2636"
+};
