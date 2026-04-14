12fa8a57f44ca3369fdfda34b27a1b94ee4a6dae
MH-Lanches <mhlanches.igarassu@gmail.com>
2026-04-14 12:10:35 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    // This project uses standalone HTML files (index.html, admin.html, pdv.html)
+    // The Vite build inlines index.html as the entry point
+  }, []);
+
+  return null;
+}
+
+export default App;
